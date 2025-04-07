194d769dd63062ba5049ad28c1d26bdc899d89d8
Ashley <ddclare126@163.com>
2025-04-07 08:13:04 +0000

    update commonScripts/EastMoneyCrawlel/spotGoldPriceCrawler/spotgold_price.py.
    Signed-off-by: Ashley <ddclare126@163.com>

--- commonScripts/EastMoneyCrawlel/spotGoldPriceCrawler/spotgold_price.py
+++ commonScripts/EastMoneyCrawlel/spotGoldPriceCrawler/spotgold_price.py
@@ -6,12 +6,9 @@ import re
 
 
 def get_gold_data():
-    headers = {
-        'User-Agent': 'Your Agent'
-    }
     now = time.time()
     url = f'https://push2.eastmoney.com/api/qt/clist/get?np=1&fltt=1&invt=2&cb=jQuery37109255212463222533_{now}&fs=m%3A118&fields=f12%2Cf13%2Cf14%2Cf1%2Cf2%2Cf4%2Cf3%2Cf152%2Cf17%2Cf28%2Cf15%2Cf16%2Cf124&fid=f3&pn=1&pz=20&po=1&dect=1&ut=fa5fd1943c7b386f172d6893dbfba10b&wbp2u=%7C0%7C0%7C0%7Cweb&_={now}'
-    response = requests.get(url, headers=headers)
+    response = requests.get(url)
     data = response.text
     r = re.compile('.*\((.*)\).*')
     data = r.findall(data)[0]
@@ -39,7 +36,5 @@ def data_process(df):
 
 
 if __name__ == '__main__':
-    df = pd.DataFrame()
     df = get_gold_data()
     df = data_process(df)
-    print(df)
