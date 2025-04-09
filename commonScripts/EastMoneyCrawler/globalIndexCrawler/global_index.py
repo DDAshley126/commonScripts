df29b216ab876a876ad2214f56cf7e7e84bdb256
Ashley <ddclare126@163.com>
2025-04-09 08:32:36 +0000

    update commonScripts/EastMoneyCrawler/globalIndexCrawler/global_index.py.
    Signed-off-by: Ashley <ddclare126@163.com>

--- commonScripts/EastMoneyCrawler/globalIndexCrawler/global_index.py
+++ commonScripts/EastMoneyCrawler/globalIndexCrawler/global_index.py
@@ -4,46 +4,65 @@ import re
 import pandas as pd
 import datetime
 
-def get_index_data(area):
-    now = time.time()
+def market_index(area: str = 'Asia'):
+    params = {
+        'np': '1',
+        'fltt': '1',
+        'invt': '2',
+        'fields': 'f12,f13,f14,f292,f1,f2,f4,f3,f152,f17,f18,f15,f16,f7,f124',
+        'fid': 'f3',
+        'pn': '1',
+        'pz': '20',
+        'po': '1',
+        'dect': '1',
+        'wep2u': '|0|0|0|web'
+    }
     match area:
         case '亚洲' | 'Asia' | '亚洲指数': 
-            url = f'https://push2.eastmoney.com/api/qt/clist/get?np=1&fltt=1&invt=2&cb=jQuery37109280084931775681_{now}&fs=i%3A1.000001%2Ci%3A0.399001%2Ci%3A0.399005%2Ci%3A0.399006%2Ci%3A1.000300%2Ci%3A100.HSI%2Ci%3A100.HSCEI%2Ci%3A124.HSCCI%2Ci%3A100.TWII%2Ci%3A100.N225%2Ci%3A100.KOSPI200%2Ci%3A100.KS11%2Ci%3A100.STI%2Ci%3A100.SENSEX%2Ci%3A100.KLSE%2Ci%3A100.SET%2Ci%3A100.PSI%2Ci%3A100.KSE100%2Ci%3A100.VNINDEX%2Ci%3A100.JKSE%2Ci%3A100.CSEALL&fields=f12%2Cf13%2Cf14%2Cf292%2Cf1%2Cf2%2Cf4%2Cf3%2Cf152%2Cf17%2Cf18%2Cf15%2Cf16%2Cf7%2Cf124&fid=f3&pn=1&pz=30&po=1&dect=1&ut=fa5fd1943c7b386f172d6893dbfba10b&wbp2u=%7C0%7C0%7C0%7Cweb'
+            params.update({'fs': 'i:1.000001,i:0.399001,i:0.399005,i:0.399006,i:1.000300,i:100.HSI,i:100.HSCEI,i:124.HSCCI,i:100.TWII,i:100.N225,i:100.KOSPI200,i:100.KS11,i:100.STI,i:100.SENSEX,i:100.KLSE,i:100.SET,i:100.PSI,i:100.KSE100,i:100.VNINDEX,i:100.JKSE,i:100.CSEALL'})    
         case '欧洲' | 'Europe' | '欧洲指数':
-            url = f'https://push2.eastmoney.com/api/qt/clist/get?np=1&fltt=1&invt=2&cb=jQuery37109280084931775681_{now}&fs=i%3A100.SX5E%2Ci%3A100.FTSE%2Ci%3A100.MCX%2Ci%3A100.AXX%2Ci%3A100.FCHI%2Ci%3A100.GDAXI%2Ci%3A100.RTS%2Ci%3A100.IBEX%2Ci%3A100.PSI20%2Ci%3A100.OMXC20%2Ci%3A100.BFX%2Ci%3A100.AEX%2Ci%3A100.WIG%2Ci%3A100.OMXSPI%2Ci%3A100.SSMI%2Ci%3A100.HEX%2Ci%3A100.OSEBX%2Ci%3A100.ATX%2Ci%3A100.MIB%2Ci%3A100.ASE%2Ci%3A100.ICEXI%2Ci%3A100.PX%2Ci%3A100.ISEQ&fields=f12%2Cf13%2Cf14%2Cf292%2Cf1%2Cf2%2Cf4%2Cf3%2Cf152%2Cf17%2Cf18%2Cf15%2Cf16%2Cf7%2Cf124&fid=f3&pn=1&pz=30&po=1&dect=1&ut=fa5fd1943c7b386f172d6893dbfba10b&wbp2u=%7C0%7C0%7C0%7Cweb'
+            params.update({'fs': 'i:100.SX5E,i:100.FTSE,i:100.MCX,i:100.AXX,i:100.FCHI,i:100.GDAXI,i:100.RTS,i:100.IBEX,i:100.PSI20,i:100.OMXC20,i:100.BFX,i:100.AEX,i:100.WIG,i:100.OMXSPI,i:100.SSMI,i:100.HEX,i:100.OSEBX,i:100.ATX,i:100.MIB,i:100.ASE,i:100.ICEXI,i:100.PX,i:100.ISEQ'})
         case '美洲' | 'America' | '美洲指数':
-            url = f'https://push2.eastmoney.com/api/qt/clist/get?np=1&fltt=1&invt=2&cb=jQuery37109280084931775681_{now}&fs=i%3A100.DJIA%2Ci%3A100.SPX%2Ci%3A100.NDX%2Ci%3A100.TSX%2Ci%3A100.BVSP%2Ci%3A100.MXX&fields=f12%2Cf13%2Cf14%2Cf292%2Cf1%2Cf2%2Cf4%2Cf3%2Cf152%2Cf17%2Cf18%2Cf15%2Cf16%2Cf7%2Cf124&fid=f3&pn=1&pz=20&po=1&dect=1&ut=fa5fd1943c7b386f172d6893dbfba10b&wbp2u=%7C0%7C0%7C0%7Cweb'
+            params.update({'fs': 'i:100.DJIA,i:100.SPX,i:100.NDX,i:100.TSX,i:100.BVSP,i:100.MXX'})
         case '澳洲' | 'Australia' | '澳洲指数':
-            url = f'https://push2.eastmoney.com/api/qt/clist/get?np=1&fltt=1&invt=2&cb=jQuery37109280084931775681_1744079388853&fs=i%3A100.AS51%2Ci%3A100.AORD%2Ci%3A100.NZ50&fields=f12%2Cf13%2Cf14%2Cf292%2Cf1%2Cf2%2Cf4%2Cf3%2Cf152%2Cf17%2Cf18%2Cf15%2Cf16%2Cf7%2Cf124&fid=f3&pn=1&pz=20&po=1&dect=1&ut=fa5fd1943c7b386f172d6893dbfba10b&wbp2u=%7C0%7C0%7C0%7Cweb'
+            params.update({'fs': 'i:100.AS51,i:100.AORD,i:100.NZ50'})
         case '其他' | 'other' | '其他指数':
-            url = f'https://push2.eastmoney.com/api/qt/clist/get?np=1&fltt=1&invt=2&cb=jQuery37109280084931775681_1744079388855&fs=i%3A100.UDI%2Ci%3A100.BDI%2Ci%3A100.CRB&fields=f12%2Cf13%2Cf14%2Cf292%2Cf1%2Cf2%2Cf4%2Cf3%2Cf152%2Cf17%2Cf18%2Cf15%2Cf16%2Cf7%2Cf124&fid=f3&pn=1&pz=20&po=1&dect=1&ut=fa5fd1943c7b386f172d6893dbfba10b&wbp2u=%7C0%7C0%7C0%7Cweb'
-    
-    response = requests.get(url)
+            params.update({'fs': 'i:100.UDI,i:100.BDI,i:100.CRB'})
+
+    url = 'https://push2.eastmoney.com/api/qt/clist/get'
+    response = requests.get(url, params=params)
     data = response.text
-    r = re.compile('.*\((.*)\).*')
-    data = r.findall(data)[0]
-    data = eval(data)
+    df = eval(data)
+    df = df['data']['diff']
+    result = []
+    for i in df:
+        dict = {
+            '最新价': i['f2'],
+            '涨跌幅(%)': i['f3'],
+            '涨跌额': i['f4'],
+            '振幅(%)': i['f7'],
+            '代码': i['f12'],
+            '品种': i['f14'],
+            '最高价': i['f15'],
+            '最低价': i['f16'],
+            '今开': i['f17'],
+            '昨收价': i['f18'],
+            '最新行情时间': i['f124'],
+        }
+        result.append(dict)
+    data = pd.DataFrame(result)
+    data['最新价'] = pd.to_numeric(data['最新价'], errors='coerce')/100
+    data['涨跌幅(%)'] = pd.to_numeric(data['涨跌幅(%)'], errors='coerce')/100
+    data['涨跌额'] = pd.to_numeric(data['涨跌额'], errors='coerce')/100
+    data['振幅(%)'] = pd.to_numeric(data['振幅(%)'], errors='coerce')/100
+    data['最高价'] = pd.to_numeric(data['最高价'], errors='coerce')/100
+    data['最低价'] = pd.to_numeric(data['最低价'], errors='coerce')/100
+    data['今开'] = pd.to_numeric(data['今开'], errors='coerce')/100
+    data['昨收价'] = pd.to_numeric(data['昨收价'], errors='coerce')/100
+    data['最新行情时间'] = pd.to_datetime(data['最新行情时间']+28800, unit='s')
+    data.fillna('-', inplace=True)
     return data
 
-def data_process(df):
-    data = pd.DataFrame([df['data']['diff'][0]])
-    for i in range(1, len(df['data']['diff'])):
-        data.loc[len(data)] = list(df['data']['diff'][i].values())
-    data.rename(columns={'f2': '最新价', 'f3': '涨跌幅', 'f4': '涨跌额', 'f7': '振幅', 'f12': '代码', 'f14': '名称', 'f15': '最高价',
-                        'f16': '最低价', 'f17': '开盘价', 'f18': '昨收价', 'f28': '昨结', 'f124': '最新行情时间'}, inplace=True)
-    data.drop(columns=['f1', 'f13', 'f152', 'f292'], inplace=True)
-    data['最新价'] = data['最新价'].apply(lambda x: round(int(x) / 100, 4) if x != '-' else x)
-    data['涨跌幅'] = data['涨跌幅'].apply(lambda x: str(round(int(x) / 100, 2)) + '%' if x != '-' else x)
-    data['涨跌额'] = data['涨跌额'].apply(lambda x: round(int(x) / 100, 2) if x != '-' else x)
-    data['振幅'] = data['振幅'].apply(lambda x: str(round(int(x) / 100, 2)) + '%' if x != '-' else x)
-    data['最高价'] = data['最高价'].apply(lambda x: round(int(x) / 100, 2) if x != '-' else x)
-    data['最低价'] = data['最低价'].apply(lambda x: round(int(x) / 100, 2) if x != '-' else x)
-    data['开盘价'] = data['开盘价'].apply(lambda x: round(int(x) / 100, 2) if x != '-' else x)
-    data['昨收价'] = data['昨收价'].apply(lambda x: round(int(x) / 100, 2) if x != '-' else x)
-    data['最新行情时间'] = data['最新行情时间'].apply(lambda x: datetime.datetime.fromtimestamp(x).strftime('%Y-%m-%d %H:%M:%S'))
-    return data
-
-def main():
-    df = get_index_data('Asia')
-    df = data_process(df)
-    return df
+if __name__ == '__main__':
+    market_index_df = market_index()
+    print(market_index_df)
