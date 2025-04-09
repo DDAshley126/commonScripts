318356a1773f2d54bf546fd9e9d5b708e772daaa
Ashley <ddclare126@163.com>
2025-04-09 01:52:49 +0000

    update commonScripts/sgeCrawler/spotgold_price.py.
    Signed-off-by: Ashley <ddclare126@163.com>

--- commonScripts/sgeCrawler/spotgold_price.py
+++ commonScripts/sgeCrawler/spotgold_price.py
@@ -5,16 +5,24 @@ import datetime
 import requests
 
 
-def main():
+def spot_quotes_sge() -> pd.DataFrame:
+    """
+    上海黄金交易所-延时行情
+    :return: 延时行情（开盘后实时更新）
+    :rtype: pd.DataFrame
+    """
     url = 'https://www.sge.com.cn/sjzx/yshqbg'
-    df = pd.DataFrame(columns=['代码', '最新价', '最高价', '最低价', '今开盘', '更新时间'])
+    df = pd.DataFrame(columns=['代码', '最新价', '最高价', '最低价', '今开盘'])
     response = requests.get(url)
     html = response.text
     soup = BeautifulSoup(html, 'html')
     table = soup.find('table')
-    now = datetime.datetime.now().replace(microsecond=0)
     for row in table.find_all('tr')[1:]:
         result = [cell.text for cell in row.find_all('td')]
-        result.append(now)
         df.loc[len(df)] = result
     return df
+
+
+if __name__ == '__main__':
+    spot_quotes_sge = spot_quotes_sge()
+    print(spot_quotes_sge)
